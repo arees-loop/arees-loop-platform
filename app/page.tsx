fe2e99f5833af063b7c4b53cf1f3e86e598f817f
Mutaz Gindeel <info@areesltd.com>
2026-09-21 18:15:42 +0300

    Fix Saudi Business Center verification seal

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import { useEffect, useRef, useState, type MouseEvent, type TouchEvent, type WheelEvent } from "react";
 
 const experiences = [
@@ -1697,15 +1698,20 @@ export default function Home() {
               />
             </div>
           </div>
-              <div
-        className="sbc-verify-seal"
-        data-token="bHNZMFR6VHFRQ095M3RmMzcwbU02QTQ9"
-        data-position="bottom-left"
-      ></div>
-      <script
-        src="https://eauthenticate.saudibusiness.gov.sa/EAuthSealApi/seal.js"
-        async
-      ></script>
+
+          <div className="mt-6 flex justify-center">
+            <div
+              className="sbc-verify-seal"
+              data-token="bHNZMFR6VHFRQ095M3RmMzcwbU02QTQ9"
+              data-position="bottom-left"
+            />
+          </div>
+
+          <Script
+            src="https://eauthenticate.saudibusiness.gov.sa/EAuthSealApi/seal.js"
+            strategy="afterInteractive"
+          />
+        </div>
       </footer>
 
     </main>
